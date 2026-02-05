78759252dbc1c9d86f1fd1ae294a315fec77964a
Palvi <palvid.1941@gmail.com>
2026-02-05 15:31:46 +0200

    Testing

--- test.py
+++ test.py
@@ -13,21 +13,21 @@ def test_initialize_library():
     assert book.author == "Robert Cecil Martin"
     assert book.available is True
 
-    def test_borrow_when_available(self):
-        book1 = Book("Data Analysis", "Dr. Anil Maheshwari")
-        available = book.borrow()
-        self.assertTrue(available is True)
-        self.assertFalse(book.available)
+def test_borrow_when_available(self):
+     book1 = Book("Data Analysis", "Dr. Anil Maheshwari")
+     available = book1.borrow()
+     self.assertTrue(available is True)
+     self.assertFalse(book1.available)
 
-    def test_borrow_when_not_available(self):
-        book2 = Book("Project Managment", "Harold Kerzner")
-        book.borrow()
-        available = book.borrow()
-        self.assertFalse(available is False)
-        self.assertFalse(book.available)
+def test_borrow_when_not_available(self):
+     book2 = Book("Project Managment", "Harold Kerzner")
+     book2.borrow()
+     available = book2.borrow()
+     self.assertFalse(available is False)
+     self.assertFalse(book2.available)
 
-    def test_return_book(self):
-        book = Book("Python", "Eric Matthes")
-        book.borrow()
-        book.return_book()
-        self.assertTrue(book.available)
+def test_return_book(self):
+    book = Book("Python", "Eric Matthes")
+    book.borrow()
+    book.return_book()
+    self.assertTrue(book.available)
